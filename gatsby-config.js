e217aec3a933d9747300d36c92f5e896d6c61b6b
tranbathanhtung <tranbathanhtung222@gmail.com>
2020-03-27 20:29:36 +0700

    add trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-123351028-2",
       },
     },
     `gatsby-plugin-feed`,
